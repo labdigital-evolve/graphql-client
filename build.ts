ae03d07a2c3c083a2f5a681b3f4b620270de6070
Boris Besemer <borisbesemer@gmail.com>
2025-04-18 16:32:24 +0200

    chore: use proper build script

--- build.ts
+++ build.ts
@@ -1,9 +1,19 @@
+import { $ } from "bun";
 import isolatedDeclarations from "bun-plugin-isolated-decl";
 
-await Bun.build({
+// Clean up dist first
+await $`rm -rf dist`;
+
+const output = await Bun.build({
   entrypoints: ["./src/server.ts", "./src/browser.ts"],
   outdir: "./dist",
   target: "node",
   sourcemap: "external",
   plugins: [isolatedDeclarations()],
 });
+
+if (output.success) {
+  console.log("Build successful");
+} else {
+  console.error("Build failed");
+}
